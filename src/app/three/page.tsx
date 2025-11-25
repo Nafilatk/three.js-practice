fa150d13e5e3a9a59f00efbad0cb130ef2f4aef8
mjasil <nafilatk7@gmail.com>
2025-11-25 12:13:12 +0530

    three

--- src/app/three/page.tsx
+++ src/app/three/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
- import * as THREE from "three"
+import * as THREE from "three"
 import { useEffect, useRef } from "react"
 
 
-export default function CubeScene(){
+export default function CubeScene() {
     const cubeRef = useRef<HTMLDivElement | null>(null)
 
-    useEffect (()=>{
+    useEffect(() => {
         if (!cubeRef.current) return
 
         const scene = new THREE.Scene()
@@ -20,46 +20,46 @@ export default function CubeScene(){
         )
         camera.position.z = 4
 
-        const renderer = new THREE.WebGLRenderer({antialias : true})
-        renderer.setSize(window.innerWidth , window.innerHeight)
+        const renderer = new THREE.WebGLRenderer({ antialias: true })
+        renderer.setSize(window.innerWidth, window.innerHeight)
 
         cubeRef.current.appendChild(renderer.domElement)
 
         const handleRESIZE = () => {
             camera.aspect = window.innerWidth / window.innerHeight
             camera.updateProjectionMatrix()
-            renderer.setSize(window.innerWidth , window.innerHeight)
+            renderer.setSize(window.innerWidth, window.innerHeight)
         }
 
-        window.addEventListener("resize" , handleRESIZE)
+        window.addEventListener("resize", handleRESIZE)
 
-        const geometry = new THREE.BoxGeometry(1,1,1)
-        const material = new THREE.MeshBasicMaterial({color : "yellow"})
-        const cube = new THREE.Mesh(geometry,material)
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        const material = new THREE.MeshBasicMaterial({ color: "yellow" })
+        const cube = new THREE.Mesh(geometry, material)
         scene.add(cube)
 
 
-        const animate = () =>{
+        const animate = () => {
             requestAnimationFrame(animate)
 
             cube.rotation.x += 0.01
             cube.rotation.y += 0.01
 
-            renderer.render(scene,camera)
+            renderer.render(scene, camera)
         }
         animate()
 
         return () => {
-            window.removeEventListener("resize",handleRESIZE)
+            window.removeEventListener("resize", handleRESIZE)
             if (cubeRef.current) cubeRef.current.removeChild(renderer.domElement)
         }
 
-    },[])
+    }, [])
 
 
-    return(
+    return (
         <div ref={cubeRef} className="w-full h-screen">
-                  
+
         </div>
     )
 }
