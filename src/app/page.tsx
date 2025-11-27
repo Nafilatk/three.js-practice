e95c6cf8d2ba0a5d974945ea886b39c7ebff7979
mjasil <nafilatk7@gmail.com>
2025-11-27 16:33:03 +0530

    threejs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Text3D, Center, Float, Environment } from "@react-three/drei";
 
-// 3D Text Component
 function ThreeDText() {
   const textRef = useRef<any>(null);
 
@@ -26,58 +25,29 @@ function ThreeDText() {
           bevelThickness={0.02}
           bevelSize={0.02}
         >
-          HELLO 3D
-          <meshStandardMaterial color="orange" />
+          Three.js
+          <meshStandardMaterial color="green" />
         </Text3D>
       </Float>
     </Center>
   );
 }
 
-// Floating Cube
-function FloatingCube() {
-  const cubeRef = useRef<any>(null);
-
-  useFrame(({ clock }) => {
-    if (cubeRef.current) {
-      cubeRef.current.rotation.x += 0.01;
-      cubeRef.current.rotation.y += 0.01;
-      cubeRef.current.position.y = Math.sin(clock.getElapsedTime()) * 0.3;
-    }
-  });
-  return (
-    <mesh ref={cubeRef} position={[0, 0, 0]}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="hotpink" />
-    </mesh>
-  );
-}
-
-// Main Scene
 export default function Home() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div >
       <Canvas camera={{ position: [5, 3, 5], fov: 60 }}>
-        {/* Background */}
         <color attach="background" args={['#1a1a2e']} />
         
-        {/* Lights */}
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} intensity={1} />
         
-        {/* Floor */}
-        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
-          <planeGeometry args={[10, 10]} />
-          <meshStandardMaterial color="#333" />
-        </mesh>
 
-        {/* 3D Objects */}
+
         <ThreeDText />
-        <FloatingCube />
         
-        {/* Controls */}
         <OrbitControls />
-        <Environment preset="sunset" />
+        <Environment preset="studio" />
       </Canvas>
     </div>
   );
