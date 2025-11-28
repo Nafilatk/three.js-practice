504c0d133eae87225bc810f3be40534bfad9a6b5
mjasil <nafilatk7@gmail.com>
2025-11-28 10:32:32 +0530

    threejs

--- src/app/day5/page.tsx
+++ src/app/day5/page.tsx
@@ -1,40 +1,38 @@
 "use client"
 import * as THREE from "three"
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { Environment, OrbitControls, Sky, useTexture } from "@react-three/drei";
 
 function RotatingCube() {
-   const ref = useRef<THREE.Mesh>(null!);
+  const ref = useRef<THREE.Mesh | null>(null);
 
+  const texture = useTexture("/globeimage.jpg"); // SVG NOT SUPPORTED
 
-  const texture=useTexture("/globe.svg")
- 
   useFrame(() => {
-    ref.current.rotation.y += 0.01;
+    if (ref.current) {
+      ref.current.rotation.y += 0.01;
+    }
   });
-  return (
 
-    <mesh  ref={ref}>
-      <sphereGeometry args={[1.1,64 ,64]} />
+  return (
+    <mesh ref={ref}>
+      <sphereGeometry args={[1.1, 64, 64]} />
       <meshStandardMaterial color="white" map={texture} />
     </mesh>
-    
   );
 }
 
 export default function App() {
   return (
-    <div className=" w-screen  h-screen">
-    <Canvas>
+    <div className="w-screen h-screen">
+      <Canvas>
         <Sky sunPosition={[100, 10, 13]} />
-        <Environment preset="sunset"/>
-      <directionalLight />
-      <RotatingCube />
-      <OrbitControls/>
-    </Canvas>
+        <Environment preset="sunset" />
+        <directionalLight />
+        <RotatingCube />
+        <OrbitControls />
+      </Canvas>
     </div>
   );
 }
-
-
